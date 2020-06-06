ea43bbe97499cba5d8d7a7a8722aa84ebd5a619b
qazwsx951 <62300263+qazwsx951@users.noreply.github.com>
2020-06-06 17:48:40 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">CRUD React</h1>
+          <h1 className="App-title">CRUD React JS</h1>
         </header>
         <div className="container">
           {
